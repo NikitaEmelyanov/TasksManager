2a7abec5ba6efde8f6c7763681daac21c8fba763
nemelianov <nikita.emelianov@freemake.com>
2018-08-24 17:46:58 +0300

    Add simple markup for adding tasks

--- TasksManager/ClientApp/src/app/add-task/add-task.component.ts
+++ TasksManager/ClientApp/src/app/add-task/add-task.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html'
 })
 
-export class AddTaskComponent {}
+export class AddTaskComponent {
+  private task: Task;
+  private http: HttpClient;
+
+  public addTask() {
+    /**this.http.post(this.baseUrl + 'api/TasksStorage', JSON.stringify(this.task));**/
+  }
+}
+
+interface Task {
+  name: string;
+  description: string;
+  priority: string;
+  timeToComplete: string;
+}
